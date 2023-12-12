fedac1fff44a7969e3ebbe0ba75df7ec2a073c47
devhoya97 <diajbh123@gmail.com>
2023-12-12 22:12:54 +0900

    feat: 지하철 역 이름이 2글자 이상이 아닐 시 예외처리 기능 구현

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,15 +1,20 @@
 package subway.domain;
 
 public class Station {
+    private static final int MIN_NAME_LENGTH = 2;
     private String name;
 
     public Station(String name) {
         this.name = name;
     }
 
+    private void validateNameLength(String name) {
+        if (name.length() < MIN_NAME_LENGTH) {
+            throw new IllegalArgumentException("역 이름의 길이가 유효하지 않습니다.");
+        }
+    }
+
     public String getName() {
         return name;
     }
-
-    // 추가 기능 구현
 }
