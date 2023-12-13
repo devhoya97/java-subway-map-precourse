a4253141ed1f6f03094e658863bee6750592c862
devhoya97 <diajbh123@gmail.com>
2023-12-13 11:28:09 +0900

    feat: 지하철 노선 이름의 길이를 검증하는 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,10 +1,17 @@
 package subway.domain;
 
 public class Line {
+    private static final int MIN_NAME_LENGTH = 2;
     private String name;
 
     public Line(String name) {
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() < MIN_NAME_LENGTH) {
+            throw new IllegalArgumentException("노선 이름의 최소 길이는 " + MIN_NAME_LENGTH + "입니다.");
+        }
     }
 
     public String getName() {
